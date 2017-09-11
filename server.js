fc5fc4bb16e28f411e3aac9f67abad850c86662d
BIJAYJHA <31011687+BIJAYJHA@users.noreply.github.com>
2017-09-12 00:27:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,9 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
     
 });
+
+
+var names=[];
 app.get('/submit-name',function(req,res){//url=submit-name?query
    //get the name from the request
    var name=req.query.name;//TODO
@@ -141,7 +144,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
+
 
 
 
